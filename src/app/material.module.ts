96ca9056e7e02cacd112d7261d836ee93ed49fca
Joe Parsley <joewparsley@gmail.com>
2018-06-10 19:32:02 -0700

    set up navbar, footer and home component for scaffolding

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,22 @@
 // Angular Material components
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
   providers: []
 })
 
