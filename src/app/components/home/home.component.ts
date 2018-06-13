8b556d118dc1efb520b09d829dd24059da94acdb
Joe Parsley <joewparsley@gmail.com>
2018-06-13 10:59:12 -0700

    add toggle functionality to favorite button on the home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  isFavorite: boolean = false;
+  favoriteIcon: string = 'favorite_border';
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  setFavorite() {
+    this.isFavorite = !this.isFavorite;
+    this.isFavorite ? this.favoriteIcon = 'favorite' : this.favoriteIcon = 'favorite_border';
+  }
 }
