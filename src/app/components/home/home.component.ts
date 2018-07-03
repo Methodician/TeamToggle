7f514a010b43d54da28a8dd5a2c04b2dd69267fc
Methodician <jacobgjohnston@gmail.com>
2018-07-03 14:22:09 -0700

    Swapped out for the one uploaded demo vid

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,10 +14,14 @@ export class HomeComponent implements OnInit, AfterViewInit {
   currentTime: number;
   videoArray: NodeListOf<HTMLVideoElement>;
   vids = {
-    vid1: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid1.mp4?alt=media&token=2b70a003-1c6f-495e-bbab-d5537c2054b3',
-    vid2: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid2.mp4?alt=media&token=b5d0f3ee-c61c-4dbb-8344-4b0487625b93',
-    vid3: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid3.mp4?alt=media&token=ea7c6076-13be-46f7-b438-1d327ba5f1c0',
-    vid4: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid4.mp4?alt=media&token=d2edbb75-0ee4-48fb-b667-f94ae6f4b969',
+    vid1: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/Nerds_View.mp4?alt=media&token=a9018082-6e7a-41f0-b917-ffc65c694865',
+    vid2: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/Nerds_View.mp4?alt=media&token=a9018082-6e7a-41f0-b917-ffc65c694865',
+    vid3: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/Nerds_View.mp4?alt=media&token=a9018082-6e7a-41f0-b917-ffc65c694865',
+    vid4: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/Nerds_View.mp4?alt=media&token=a9018082-6e7a-41f0-b917-ffc65c694865',
+    // vid1: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid1.mp4?alt=media&token=2b70a003-1c6f-495e-bbab-d5537c2054b3',
+    // vid2: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid2.mp4?alt=media&token=b5d0f3ee-c61c-4dbb-8344-4b0487625b93',
+    // vid3: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid3.mp4?alt=media&token=ea7c6076-13be-46f7-b438-1d327ba5f1c0',
+    // vid4: 'https://firebasestorage.googleapis.com/v0/b/team-toggle.appspot.com/o/vid4.mp4?alt=media&token=d2edbb75-0ee4-48fb-b667-f94ae6f4b969',
   };
 
 
